0374ea702064695701054dfdacc1dbeae1a3f81e
Lucila Linares <lucila.linares06@gmail.com>
2021-12-02 21:47:17 -0800

    completed Assignment

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,38 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Lucila Linares" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
-let gb = 0      // Gingerbread
-let cc = 0      // Chocolate Chip
-let sugar = 0   // Sugar Sprinkle
+let gb = 0;
+plusbutton.addEventListener('click',function(){
 
+    qty-gb + = 1;
+    document.getElementById('credit').textcontent = quantity;
+    })  
+    minusbutton.addEventListener('click',function(){
+if(qty-gb >0) {
+        qty-gb - = 1;
+]
+        document.getElementById('credit').textcontent = quantity;   // Gingerbread
+let cc = 0;     // Chocolate Chip
+
+let sugar = 0;  // Sugar Sprinkle
+plusbutton.addEventListener('click',function(){
+
+    qty-sugar  + = 1;
+    document.getElementById('credit').textcontent = quantity;
 // Code to update name display 
-document.getElementById('credit').textContent = `Created by ${yourName}`
-
+document.getElementById('credit').textContent = `Created by ${Lucila Linares}`
 // Event listener for clicks on the "+" button for Gingerbread cookies
 document.getElementById('add-gb').addEventListener('click', function() {
     // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
-
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+    let plusButton = document.queryselector(add-gb)
+    let munisbutton= document.queryselector(minus-gb)
 })
+
+   document.addEventListener('click', function(){
+     console.log('Gingerbread + button was clicked!')  
+   }
 
 // TODO: Hook up event listeners for the rest of the buttons
