0d8cffffde29fb09b30f052c2acdcae01b6dac38
Lucila Linares <lucila.linares06@gmail.com>
2021-12-07 19:59:52 -0800

    Adding my profile so it can be published!

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,57 @@
-// HINT: You can delete this console.log after you no longer need it!
+let plusButton = document.queryselector(add-gb, add-cc, add-sugar)
+let munisbutton= document.queryselector(minus-gb, minus-cc, minus-sugar)// HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
 let yourName = "Lucila Linares" // HINT: Replace this with your own name!
-
+ 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0;
-plusbutton.addEventListener('click',function(){
-
-    qty-gb + = 1;
+    plusbutton.addEventListener('click',function(){
+        qty-gb + = 1;
     document.getElementById('credit').textcontent = quantity;
     })  
     minusbutton.addEventListener('click',function(){
-if(qty-gb >0) {
+    if(qty-gb >0) {
         qty-gb - = 1;
-]
         document.getElementById('credit').textcontent = quantity;   // Gingerbread
-let cc = 0;     // Chocolate Chip
 
-let sugar = 0;  // Sugar Sprinkle
+ let cc = 0; 
+        plusbutton.addEventListener('click',function(){
+
+        qty-cc + = 1;
+         document.getElementById('credit').textcontent = quantity;
+         })  
+         minusbutton.addEventListener('click',function(){
+         if(qty-cc >0) {
+          qty-cc - = 1;
+       document.getElementById('credit').textcontent = quantity;     // Chocolate Chip
+
+let sugar = 0; 
 plusbutton.addEventListener('click',function(){
-
-    qty-sugar  + = 1;
+    qty-sugar + = 1;
     document.getElementById('credit').textcontent = quantity;
+    })  
+    minusbutton.addEventListener('click',function(){
+    if(qty-sugar >0) {
+        qty-sugar - = 1;
+        document.getElementById('credit').textcontent = quantity;   // Sugar Sprinkle
+document.addEventListener('click', function)}
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${Lucila Linares}`
-// Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    let plusButton = document.queryselector(add-gb)
-    let munisbutton= document.queryselector(minus-gb)
-})
 
-   document.addEventListener('click', function(){
-     console.log('Gingerbread + button was clicked!')  
-   }
+document.getElementById('add-gb').addEventListener('click', function() {
+    document.addEventListener('click', function(){
+    console.log('Gingerbread + button was clicked!')
+   
+    document.getElementById('add-cc').addEventListener('click', function() {
+        document.addEventListener('click', function(){
+        console.log('Chocolatechip + button was clicked!')
+       
+        document.getElementById('add-sugar').addEventListener('click', function() {
+            document.addEventListener('click', function(){
+            console.log('Sugar + button was clicked!')
+    // HINT: You can delete this console.log after you no longer needed  
+    }
 
 // TODO: Hook up event listeners for the rest of the buttons
